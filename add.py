5b35cf009e08b53ebeede2fd02fffeb4f0ec3139
Demo User <demouser@MacBook-Pro.local>
2017-07-31 10:37:30 -0700

    add addpet.html

--- add.py
+++ add.py
@@ -8,5 +8,13 @@ jinja_env = jinja2.Environment(
 
 class addPet(webapp2.RequestHandler):
     def post(self):
-        super(addPet, self).__init__()
-        self.arg = arg
+        
+        my_vars = {
+            "noun1":self.request.get("noun1"),
+            "activity":self.request.get("activity"),
+            "celebrity":self.request.get("celebrity"),
+            "teacher":self.request.get("teacher"),
+            "show":self.request.get("show"),
+            "fun":self.request.get("fun")
+        }
+        self.response.out.write("You have submitted your madlib")
