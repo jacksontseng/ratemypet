20bc9f1448e14cc2fd908b8d3f730da5e5c46f27
Demo User <demouser@MacBook-Pro.local>
2017-08-02 09:16:05 -0700

    force push wed morning

--- add.py
+++ add.py
@@ -19,9 +19,9 @@ class AddPet(webapp2.RequestHandler):
         description = self.request.get("description")
         date = self.request.get('time_posted')
         age = int(self.request.get("age"))
-        # add image url here
+        image = self.request.get('image')
 
-        new_pet(petname, atype, breed, description, age, date,) #add image argument
+        new_pet(petname, atype, breed, description, age, date, image) #add image argument
         self.response.out.write("You have submitted your pet <br>")
         self.response.out.write("<a href='/addpet'> add another pet</a>") #sketch way of adding html thru python
         # self.redirect('/')
@@ -34,12 +34,12 @@ class AddPet2DS(ndb.Model):
     time_posted = ndb.DateTimeProperty()
     description = ndb.StringProperty()
     age = ndb.IntegerProperty()
-    # add image here
+    image = ndb.StringProperty()
 
 
-def new_pet(petname, atype, breed, description, age, time_posted): #add image argument
+def new_pet(petname, atype, breed, description, age, time_posted, image): #add image argument
   """Puts a new pet into Datastore."""
-  newpet = AddPet2DS(petname=petname, age=age, atype=atype, time_posted=datetime.datetime.now(), breed=breed, description = description)
+  newpet = AddPet2DS(petname=petname, age=age, atype=atype, time_posted=datetime.datetime.now(), breed=breed, description = description, image=image)
   return newpet.put()
 
 logging.info(AddPet2DS.query().get())
